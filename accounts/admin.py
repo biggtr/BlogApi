d731d2ccd0ff055c0452977d7ec152067f7f59bc
bigtorresg <bigtorresg@gmail.com>
2023-09-18 15:35:29 +0300

    First init

---
+++ accounts/admin.py
@@ -0,0 +1,66 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+# Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "first_name",
+        "is_staff",
+        "is_active",
+    ]
+    list_filter = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (
+            "Credentials",
+            {
+                "fields": (
+                    "username",
+                    "email",
+                    "age",
+                    "password",
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide"),
+                "fields": (
+                    "username",
+                    "email",
+                    "age",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
